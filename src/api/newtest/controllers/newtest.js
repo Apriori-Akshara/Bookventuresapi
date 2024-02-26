2b211d6de8ba8e2be6434a2098443a370c8477b4
PushkarGaur10 <gaurpushkar10@gmail.com>
2024-02-26 17:18:58 +0530

    first commit

---
+++ src/api/newtest/controllers/newtest.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * newtest controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::newtest.newtest');
